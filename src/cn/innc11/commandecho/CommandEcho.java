348df61e6595555968bdbf70470128c9065f9030
innc11 <pm@innc11.cn>
2019-12-10 16:12:07 +0800

    second commit

--- src/cn/innc11/commandecho/CommandEcho.java
+++ src/cn/innc11/commandecho/CommandEcho.java
@@ -1,31 +1,45 @@
 package cn.innc11.commandecho;
 
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.command.RemoteConsoleCommandSender;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerCommandPreprocessEvent;
 import cn.nukkit.event.server.RemoteServerCommandEvent;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.scheduler.Task;
+import cn.nukkit.utils.LogLevel;
 import cn.nukkit.utils.TextFormat;
 
 public class CommandEcho extends PluginBase implements Listener
 {
-	
 	@Override
 	public void onEnable() 
 	{
 		getServer().getPluginManager().registerEvents(this, this);
 	}
-	
-	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = false)
+
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onRemoteServerCommand(RemoteServerCommandEvent e)
 	{
-		getLogger().info("RCON Command: "+e.getCommand());
+		RemoteConsoleCommandSender sender = (RemoteConsoleCommandSender) e.getSender();
+		rawLog("&3RCON Command: "+e.getCommand());
 	}
 	
-	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = false)
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent e)
 	{
-		getLogger().info(TextFormat.colorize((e.getPlayer().isOp() ? "&c" : "")+e.getPlayer().getName()+(e.getPlayer().isOp() ? "&r" : ""))+ ": "+e.getMessage());
+		Player player = e.getPlayer();
+		String playName = player.getName();
+		boolean isOp = player.isOp();
+
+		rawLog( (isOp? "&c":"") + playName + (isOp? "&r: ":": ") + e.getMessage());
+	}
+
+	public void rawLog(String message)
+	{
+		Server.getInstance().getLogger().log(LogLevel.INFO, TextFormat.colorize(message));
 	}
 }
